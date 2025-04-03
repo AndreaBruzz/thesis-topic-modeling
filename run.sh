8339c9f03f71ed3f3e9e346de8e321a82f2a1669
Andrea Bruttomesso <andrea.bruttomesso.1@studenti.unipd.it>
2025-04-03 15:30:24 +0200

    improve run script

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-python3 main.py -ri tipster_45_kstem -fm Qrels -vc "Terms window" -et "Frozen Ranking" -em embedding_full -md NMF -tp join
+DOCKER="docker compose up -d"
+CODE="python3 main.py -ri tipster_45_kstem -fm Qrels -vc \"Terms window\" -et \"Frozen Ranking\" -em embedding_full -md NMF -tp join"
+
+echo $DOCKER
+$DOCKER
+
+echo $CODE
+eval $CODE
